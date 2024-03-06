b76ba07f8c12b6b96dfabc5e0afcc91f10387c29
Domiidp04 <domiddp2004@gmail.com>
2024-03-06 19:48:42 +0100

    Subida de Proyecto

---
+++ src/test/java/TFG_Ezyshop_Backend/TFG_Ezyshop_Backend/EzyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package TFG_Ezyshop_Backend.TFG_Ezyshop_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzyshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
