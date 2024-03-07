4c724b599dd26a778eb65a3bc191a0ebee4debb9
Domiidp04 <domiddp2004@gmail.com>
2024-03-07 20:34:58 +0100

    Creacion de entidad User

--- src/main/java/TFG_Ezyshop_Backend/Entity/User.java
+++ src/main/java/TFG_Ezyshop_Backend/Entity/User.java
@@ -1,5 +1,47 @@
 package TFG_Ezyshop_Backend.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table( name = "Users" )
 public class User {
+
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY )
+	private long id;
+	
+	private String username;
+	
+	private String password;
+	
+	private String email;
+	
+	private boolean locked;
+	
+	private boolean disabled;
+	
+	private String name;
+	
+	private String lastname;
+	
+	private String zip;
+	
+	private String street;
+	
+	private String number;
+	
+	private String description;
+	
+	@ManyToOne
+	@JoinColumn( name = "role_id", insertable = false, updatable = false )
+	private Role role;
 
 }
