f2ee0c04adb5af2d9601dbd9018a61de1d482f73
Domiidp04 <domiddp2004@gmail.com>
2024-03-14 21:58:08 +0100

    fatal

--- src/main/java/TFG_Ezyshop_Backend/Controllers/ProductController.java
+++ src/main/java/TFG_Ezyshop_Backend/Controllers/ProductController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import TFG_Ezyshop_Backend.Dto.AdminProductDto;
+import TFG_Ezyshop_Backend.Dto.ProductDto;
+import TFG_Ezyshop_Backend.Entity.Product;
 import TFG_Ezyshop_Backend.Services.ProductService;
 
 @RestController
@@ -22,11 +24,18 @@ private final ProductService productService;
 	}
 	
 	@GetMapping
-	public ResponseEntity<List<AdminProductDto>> getAll(){
+	public ResponseEntity<List<ProductDto>> getAll(){
 		System.out.println(productService.getAll());
 		System.out.println("hola");
-		return new ResponseEntity<List<AdminProductDto>>(productService.getAll(), HttpStatus.OK);
+		return new ResponseEntity<List<ProductDto>>(productService.getAll(), HttpStatus.OK);
 	}
+	
+	@GetMapping("/{productId}")
+    public ResponseEntity<Product> getProduct(@PathVariable("productId") long productId) {
+    	return productService.getById(productId)
+				.map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+				.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 	
 	
 }
