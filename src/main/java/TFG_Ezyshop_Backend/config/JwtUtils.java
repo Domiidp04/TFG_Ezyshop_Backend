34b0645d5710082b8e49fd9a08f76bce1f2cd120
Domiidp04 <domiddp2004@gmail.com>
2024-04-03 17:17:50 +0200

    Cambio a variables de entorno la key jwt

--- src/main/java/TFG_Ezyshop_Backend/config/JwtUtils.java
+++ src/main/java/TFG_Ezyshop_Backend/config/JwtUtils.java
@@ -3,6 +3,7 @@ package TFG_Ezyshop_Backend.config;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.auth0.jwt.JWT;
@@ -12,13 +13,14 @@ import com.auth0.jwt.exceptions.JWTVerificationException;
 @Component
 public class JwtUtils {
 
-	private static String SECRET_KEY = "superdaw";
+	@Value("${key.jwt}")
+	private static String SECRET_KEY;
 	private static Algorithm ALGORITHM = Algorithm.HMAC256(SECRET_KEY);
 	
 	public String create(String username) {
 		return JWT.create()
 				.withSubject(username)
-				.withIssuer("superdaw")
+				.withIssuer(SECRET_KEY)
 				.withIssuedAt(new Date())
 				.withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
 				.sign(ALGORITHM);
