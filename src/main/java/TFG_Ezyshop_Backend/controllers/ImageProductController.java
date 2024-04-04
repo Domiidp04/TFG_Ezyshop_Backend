34c57a9fc04042118a3e83373518364e622ae576
Domiidp04 <domiddp2004@gmail.com>
2024-04-04 13:34:41 +0200

    Arreglo de controlador

--- src/main/java/TFG_Ezyshop_Backend/controllers/ImageProductController.java
+++ src/main/java/TFG_Ezyshop_Backend/controllers/ImageProductController.java
@@ -1,9 +1,13 @@
 package TFG_Ezyshop_Backend.controllers;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,16 +24,30 @@ public class ImageProductController {
     private ImageProductService imageProductService;
     
     public ImageProductController(ImageProductService imageProductService) {
+    	this.imageProductService = imageProductService;
     }
 
-//    @PostMapping("/upload")
-//    public ResponseEntity<ImageProduct> uploadImage(@RequestParam("productId") Long productId, @RequestParam("image") MultipartFile file) {
-//    	try {
-//    	    ImageProduct imageProduct = imageProductService.saveImage(productId, file);
-//            return new ResponseEntity<>(imageProduct, HttpStatus.OK);
-//    	} catch (IOException e) {
-//    	    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//    	}
+
+    @PostMapping("/upload/{productId}")
+    public ResponseEntity<ImageProduct> uploadImage(@PathVariable Long productId, @RequestParam("imageFile") MultipartFile imageFile) {
+        try {
+            ImageProduct imageProduct = imageProductService.saveImage(imageFile, productId);
+            return new ResponseEntity<>(imageProduct, HttpStatus.CREATED);
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+//    @GetMapping("/product/{productId}")
+//    public ResponseEntity<List<ImageProduct>> getImagesByProductId(@PathVariable Long productId) {
+//        List<ImageProduct> images = imageProductService.getImagesByProductId(productId);
+//        return new ResponseEntity<>(images, HttpStatus.OK);
 //    }
+
+    @DeleteMapping("/delete/{imageId}")
+    public ResponseEntity<Void> deleteImage(@PathVariable Long imageId) {
+        imageProductService.deleteImage(imageId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
